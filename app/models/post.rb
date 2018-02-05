5f26244143b5e89b3d81614ae4f764e655041d23
Hoon Lee <yl.hoony@gmail.com>
2018-02-05 20:14:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,14 +10,14 @@ class Post < ActiveRecord::Base
 
   def tags_attributes=(tags_attributes)
     # binding.pry
-    if tags_attributes.present?
+    # if tags_attributes.present?
       tags_attributes.values.each do |tag_attributes|
         # binding.pry
         if tag_attributes[:name].present?
           self.tag = Tag.create(name: tag_attributes[:name], post_id: self)
         end
       end
-    end
+    # end
   end
 
 end
